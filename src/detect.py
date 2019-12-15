55d2063b2fdb8f8fd02550d72bfd79d215223eca
Xiaocong <leon941213yu@hotmail.com>
2019-12-14 21:43:03 -0600

    update

--- src/detect.py
+++ src/detect.py
@@ -10,6 +10,7 @@ from PIL import Image
 import cv2
 import numpy as np
 import random
+import time 
 
 '''get_mask'''
 def get_mask_gray(prediction, src_img, th_mask, th_score):
@@ -45,7 +46,8 @@ def get_mask_gray(prediction, src_img, th_mask, th_score):
     mask threshold:     deafult set to be 0.5 for soft mask border
     class_num:          default set to be 91 categories 
 '''
-def do_main(model, data=None, output_path=None, priorities=None, th_mask=0.5, th_scores=0.75, class_num=91):
+def do_main(env, data=None, output_path=None, priorities=None, th_mask=0.5, th_scores=0.75, class_num=91):
+    model = env.model
     src_img = None
     if data is not None:
         src_img = data
@@ -55,14 +57,17 @@ def do_main(model, data=None, output_path=None, priorities=None, th_mask=0.5, th
         raise RuntimeError("invalid data input") 
 
     if model is None:
+        print("model initialized")
         model = detection.maskrcnn_resnet50_fpn(num_classes=91, pretrained=True)
         model.eval()
+        env.model = model
     # in_features = model.roi_heads.box_predictor.cls_score.in_features 
     # model.roi_heads.box_predictor = detection.faster_rcnn.FastRCNNPredictor(in_features, class_num)
     
     masked_img=None
     masks=None
     masks = []
+    tsum = 0
     for idx in range(len(src_img)):
         if idx == priorities[0]:
             masks.append(None)
@@ -73,7 +78,9 @@ def do_main(model, data=None, output_path=None, priorities=None, th_mask=0.5, th
         # print("index for model: ", idx)
         img = torch.from_numpy(np.expand_dims(src_img[idx]/255., 2)).permute(2,0,1).float()
         input_img.append(img)
+        t = time.time()
         prediction = model(input_img)
+        tsum += (time.time() - t) 
         mask = get_mask_gray(prediction, src_img[idx], th_mask, th_scores)
         masks.append(mask)
-    return masks
+    return masks, tsum
--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import masks
 import detect
 import os
 import glob
-
+import time
 
 class work_env:
     
@@ -16,7 +16,7 @@ class work_env:
         self.dir_path = dir_path
         self.data_path = data_path
         self.video_fps = video_fps
-        self.frame_interval = 10 # framing frequency
+        self.frame_interval = 1 # framing frequency
         self.homo_invs, self.transforms =  masks.preprocess(self.homos)
         self.video_filepath = []
         self.frames = []
@@ -26,7 +26,7 @@ class work_env:
         self.produced_frame_count = 0
 
     '''preprocessing: frame the video into frames'''
-    def get_frames(self, offset_frames):
+    def get_frames(self, offset_frames, tagid):
         # fetch next frame
         if (len(self.frames) != 0):
             return (self.frames[0].get(cv2.CAP_PROP_FRAME_WIDTH), self.frames[0].get(cv2.CAP_PROP_FRAME_HEIGHT), len(self.frames))
@@ -47,7 +47,7 @@ class work_env:
             height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
             width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
             # filename, fourcc-four char code of codec, fps, frameSize, [, isColor] 
-            output_path = os.path.join(self.data_path, 'output-' + os.path.basename(video))
+            output_path = os.path.join(self.data_path, 'output-' + str(tagid) + '-' + os.path.basename(video))
             out = cv2.VideoWriter(output_path, fourcc, self.video_fps, (width, height), False)
             print("video path: {0}, capture openable: {1}, h & w: {2} {3}, output_path: {4}".format(video, cap.isOpened(), height, width, output_path))
             # add to array of frames
@@ -73,9 +73,27 @@ class work_env:
             self.output_frames[idx].release()
         print('video pos', self.video_pos)
         
-        
-def main():
-    print("program launched with {0} arguments".format(len(sys.argv) - 1))
+
+TAG_TEST = 2
+TAG_NORMAL = 1
+DUR_SETUP = "setup the environment"
+DUR_TRANS_MASK_CREATION = "create transform area mask"
+DUR_INPUT_REDUCE = "create reduced input"
+DUR_HUMAN_SEGMENTATION = "create segmentation mask for overlap area"
+DUR_OVERLAP = "create the synthesized output image"
+DUR_MODEL_PREDICTION = "DNN model prediction(Part of Upper Item - Segmentation mask creation)"
+DUR_WHOLE = "overall runtime: "
+DUR_ARR = [DUR_SETUP, DUR_TRANS_MASK_CREATION, DUR_INPUT_REDUCE< DUR_HUMAN_SEGMENTATION, DUR_MODEL_PREDICTION, DUR_OVERLAP]
+
+t_map = {}
+t_map[DUR_INPUT_REDUCE] = 0
+t_map[DUR_HUMAN_SEGMENTATION] = 0
+t_map[DUR_OVERLAP] = 0
+t_map[DUR_MODEL_PREDICTION] = 0
+def main(offset_frames):
+    print("start build up environment and launch")
+    start_time = time.time()
+    curr_time = start_time
     # calibration input for four views, TODO replace stat with outsource
     calib_stat = masks.stat()
     curr_dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -83,13 +101,19 @@ def main():
     video_fps = 20
     ENV = work_env(calib_stat, curr_dir_path, video_path, video_fps)
 
+    tmp_time = time.time()
+    t_map[DUR_SETUP] = tmp_time - curr_time
+    curr_time = tmp_time
+
     # pipeline for each frame
     priorities = ENV.get_priorities()
     transforms = ENV.transforms
     homos = ENV.homos
     homo_invs = ENV.homo_invs 
     print("get frame from videos")
-    stats = ENV.get_frames(400)
+
+    # offset of the original videos
+    stats = ENV.get_frames(offset_frames, TAG_NORMAL)
     width = stats[0]
     height = stats[1]
     n = stats[2] # n is the number of views, usually 4 for our dataset
@@ -101,7 +125,10 @@ def main():
     # calculate the static masks
     print("start create overlap mask for frames, shape of transforms={0}, shape of priorities={1}".format(np.shape(transforms), np.shape(priorities)))
     stitch_masks = masks.create_img_mask(height, width, n, transforms, priorities)
-    
+    tmp_time = time.time()
+    t_map[DUR_TRANS_MASK_CREATION] = tmp_time - curr_time
+    curr_time = tmp_time
+
     ENV.produced_frame_count = 0
     duration = 1
     output_frames = duration * ENV.video_fps # output duration = output frames / 20fps 
@@ -129,16 +156,25 @@ def main():
             print("frame lost for video in {0}th framing".format(ENV.produced_frame_count))
             continue
         
-        # overlap input images
+        # use only overlapped area to do human segmentation
         overlapped_input_images = []
         for idx in range(n):
             if idx == priorities[0]:
                 overlapped_input_images.append(None)
                 continue
             overlapped_input_images.append(cv2.bitwise_and(src1=input_images[idx], src2=(255 - stitch_masks[idx]).astype('uint8')).astype('uint8'))
+        
+        segmented_masks, tmodel = detect.do_main(ENV, data=overlapped_input_images, priorities=priorities)
+        t_map[DUR_MODEL_PREDICTION] += tmodel
+        tmp_time = time.time()
+        t_map[DUR_HUMAN_SEGMENTATION] += (tmp_time - curr_time)
+        curr_time = tmp_time
 
-        segmented_masks = detect.do_main(ENV.model, data=overlapped_input_images, priorities=priorities)
         reduced_input_images = []
+
+        # TODO 
+        # 1, compare video size for same length of video
+        # 2, calculate time cost for diff phrases
         for idx in range(n):
             if idx == priorities[0]: 
                 reduced_input_images.append(input_images[idx])
@@ -146,10 +182,20 @@ def main():
             # reverse mask
             reduced_input_images.append(cv2.bitwise_and(src1=input_images[idx], src2=(stitch_masks[idx]).astype('uint8')))
             # cv2.imwrite('./reduced-' + str(idx) + '.png', reduced_input_images[idx])
-            
+        tmp_time = time.time()
+        t_map[DUR_INPUT_REDUCE] += (tmp_time - curr_time)
+        curr_time = tmp_time
+        '''    
         # start video manipulation, apply to each frame 
         output_images = masks.transform_n_crop(reduced_input_images, priorities, stitch_masks, homos, homo_invs)
         output_images = masks.overlap_images(output_images, priorities, segmented_masks, input_images) # overlap the segmented pic onto the transformed image
+        '''
+        # test use 
+        output_images = reduced_input_images
+        output_images = masks.overlap_images(output_images, priorities, segmented_masks, input_images) # overlap the segmented pic onto the transformed image
+        tmp_time = time.time()
+        t_map[DUR_OVERLAP] += (tmp_time - curr_time)
+        curr_time = tmp_time
 
         # np.set_printoptions(threshold=sys.maxsize)
         for idx in range(n):
@@ -164,7 +210,81 @@ def main():
         
     # remove all 
     ENV.release_all()
+    overall_runtime = time.time() - start_time
+    print("Tideo has been generated\nvideo offset:\t400\noutput dir:\t{0}\nduration:\t{1}s\nfps:\t{2}\n".format(ENV.data_path, duration, ENV.video_fps))
+    print("Time cost for different stage:")
+    for event in DUR_ARR:
+        print("Action: {0} \t Cost: {1}s \t Percentage: {2}% ".format(event, t_map[event], t_map[event] * 100 / overall_runtime))
+    print("------------------------------------------")
+    print("Overall Runtime: ", overall_runtime)
+
+def test(offset_frames):
+    
+    print("program launched with {0} arguments".format(len(sys.argv) - 1))
+    # calibration input for four views, TODO replace stat with outsource
+    calib_stat = masks.stat()
+    curr_dir_path = os.path.dirname(os.path.realpath(__file__))
+    video_path = os.path.join(curr_dir_path, "../data/")
+    video_fps = 20
+    ENV = work_env(calib_stat, curr_dir_path, video_path, video_fps)
+
+    # pipeline for each frame
+    priorities = ENV.get_priorities()
+    transforms = ENV.transforms
+    homos = ENV.homos
+    homo_invs = ENV.homo_invs 
+    print("get frame from videos")
+
+    # offset of the original videos
+    stats = ENV.get_frames(offset_frames, TAG_TEST)
+    width = stats[0]
+    height = stats[1]
+    n = stats[2] # n is the number of views, usually 4 for our dataset
+    print("val: {0} {1} {2}".format(width, height, n))
+
+    frames = ENV.frames
+    output_frames = ENV.output_frames
+
+    ENV.produced_frame_count = 0
+    duration = 1
+    output_frames = duration * ENV.video_fps # output duration = output frames / 20fps 
+    while(ENV.produced_frame_count < output_frames):
+        print("start handling frames from {0}/total_frames of the input".format(ENV.video_pos[0]))
+        ret = False
+        input_images = []
+    
+        for idx in range(n):
+            frames[idx].set(1, ENV.video_pos[idx])
+            if not frames[idx].isOpened():
+                print("{0}th file is not openable".format(idx))
+                exit(1)
+            ret, frame = frames[idx].read()
+            if frame is None or not ret:
+                print("frame lost for {0}th video, exit from the main loop".format(idx))
+                break
+            input_images.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+        
+        # update position of video
+        for idx in range(n):
+            ENV.video_pos[idx] += ENV.frame_interval
+        
+        if not ret: 
+            print("frame lost for video in {0}th framing".format(ENV.produced_frame_count))
+            continue
+        
+        for idx in range(n):
+            ENV.output_frames[idx].write(input_images[idx]) 
+       
+        if ret == False: 
+            break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break    
+        ENV.produced_frame_count+=1
+        
+    # remove all 
+    ENV.release_all()
     print("video has been generated\nvideo offset:\t400\noutput dir:\t{0}\nduration:\t{1}s\nfps:\t{2}\n".format(ENV.data_path, duration, ENV.video_fps))
-   
+    print()
 if __name__ == '__main__':
-    main() 
+    main(400) 
+    # test(400)
